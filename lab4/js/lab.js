37099831ea2596db870dcb9cbcf080b3cbb166ee
simonatrent <simonatrentchev@gmail.com>
2024-10-17 23:18:24 -0700

    Update lab.js

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,7 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+//Rock Paper Scissors
+//An automated game
+//Simona Trentchev <strentch@ucsc.edu>
+//October 17 2024
 
+<pre><code>
+//Psudocode
+  //Rock Paper Scissors
+    // Create three buttons
+    // Make buttons interactive so an option can be chosen
+  // When option is chosen by p1 generate a response page
+// If p1 selects rock and computer choses scissors;  p1 wins
+    //If p1 selects rock and computer choses paper; computer wins
+  //If p1 selects rock and computer choses rock; draw
+//If p1 selects paper and computer choses scissors; computer wins
+  // If p1 selects paper and computer choses paper; draw
+    // If p1 selects paper and computer choses rock; p1 wins
+  // If p1 selects scissors and computer choses scissors; draw
+// If p1 selects scissors and computer choses paper; p1 wins
+  // If p1 selects scissors and computer choses rock; computer wins
+
+  </pre></code>
 // Constants
 
 // Functions
