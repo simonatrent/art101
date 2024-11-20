6ecffe6b126455d749e1f7afc606c43223cd5ffb
simonatrent <simonatrentchev@gmail.com>
2024-11-20 11:46:23 -0800

    yay

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,24 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Simona Trentchev <strentch@ucsc.edu>
+// Created: 10/22/24
+//license: Public Domain
 
 // Constants
+make = "subaru";
+model = "crosstreck";
+color = "red";
+year = 2015
 
 // Functions
+age = 2024 - year;
+
+//outputs
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
