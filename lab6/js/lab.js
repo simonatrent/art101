ac903234b05df6fb9f68c208c194372cc0a657e9
simonatrent <simonatrentchev@gmail.com>
2024-11-20 13:32:17 -0800

    meow

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,9 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Arrays and Objects
+// Author: Simona Trentchev
+// Date: 10/28/24
 
 // Constants
+let myTransportation= ("Subaru","walk","bus","bike");
+let myMainRide = (
+  make: "Subaru",
+  model: "Crosstreck",
+  color: "red",
+  year: 2015,
+  age: function () {
+      return 2024 - this.year;
+  }
+);
 
+// output
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
