da0cb7144e9798f8a8b9f7cf6f1615723f45c403
simonatrent <simonatrentchev@gmail.com>
2024-11-25 12:06:04 -0800

    i

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -14,11 +14,11 @@ console.log("Meowww2", Meowww(2));-
 array [10,14,166,39,68]- 
 console.log("My array", array);-
 
- result = array.map(Meowww);- 
+var result = array.map(Meowww);- 
 // should return [100, 196, 27556, 1521, 4624]
 
 console.log("Test of squarness of array:", result);
 
- result = array.map(function(x){-
+var result = array.map(function(x){-
  return x * x;-
 })-
