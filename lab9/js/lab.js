c71c0572ee24f7b05ad2cf876a5b7669f6d8a411
simonatrent <simonatrentchev@gmail.com>
2024-11-25 14:48:02 -0800

    md

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -17,10 +17,28 @@ $("#button-challenge").click(function(){
 });
 
 // add button to problems section
-$("#problems").append("<button id='button-problems'>Special</button>");
+$("#problems").append("<button id='button-problems'>Kurpow</button>");
 
 // add a click listener to the problems button
 $("#button-problems").click(function(){
     // now add (or subtract) the "special" class to the section
-    $("#problems").toggleClass("special");
+    $("#problems").toggleClass("kurpow");
+});
+
+// add button to refection section
+$("#refection").append("<button id='button-refection'>Swoosh</button>");
+
+// add a click listener to the reflection button
+$("#button-reflectione").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#reflection").toggleClass("swoosh");
+});
+
+// add button to results section
+$("#results").append("<button id='button-results'>Zam</button>");
+
+// add a click listener to the results button
+$("#button-results").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#results").toggleClass("zam");
 });
